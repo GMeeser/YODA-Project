f78d58b491c299fbece7796b0bc7d88d48e65f0d
Grant <>
2020-06-25 12:48:57 +0200

    Python test bench with golden measure and serial communication | Grant

---
+++ Grant's Code/functions.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 24 16:28:15 2020
+
+@author: Tim
+"""
+from PIL import Image
+
+def integerArrayToByteArray(integer_array):
+    byte_array = []
+    for i in integer_array:
+        byte_array.append(i.to_bytes(1,'big'))
+
+    return byte_array
+
+def byteArrayToIntegerArray(byte_array):
+    integer_array = []
+    for i in byte_array:
+        integer_array.append(int.from_bytes(i,'big'))
+
+    return integer_array
+
+def imageToArray(im_file):
+    im = Image.open(im_file)
+    #List of Tuples
+    pix_val = list(im.getdata())
+    number_in_tuple = len(pix_val[0])
+    #1D list
+    pix_val_flat = [x for sets in pix_val for x in sets]
+    #To access use pix_val_flat, number_in_tuple = imageToArray(the_image.bmp)
+    return integerArrayToByteArray(pix_val_flat), number_in_tuple
+
+def arrayToImageArray(encrypted_array, number_in_tuple):
+    encrypted_array = byteArrayToIntegerArray(encrypted_array)
+    #Encrypted List
+    encrypted_image_array = []
+    for i in range(0,len(encrypted_array),number_in_tuple):
+        #Temporary list that will be tupled
+        lst = []                                            
+        #Add the correct number of data points to each tuple. Some image types have RGB some have RGBA etc
+        for j in range(0,number_in_tuple):
+            lst.append(encrypted_array[i+j])
+        #Make list into tuple
+        tup = tuple(lst)
+        #Add each tuple to the encrypted pixel list
+        encrypted_image_array.append(tup)                                 
+        
+    return(encrypted_image_array)
+
+def arrayToImage(encrypted_image_array,unencrypted_image_file):
+    #Open Original image to get size and colour depth
+    im = Image.open(unencrypted_image_file)
+    #Make second image same size as first
+    im2 = Image.new(im.mode, im.size)
+    #Convert byte array to image data array
+    encrypted_image_array = arrayToImageArray(encrypted_image_array, len(im.mode))
+    #Load pixle data into image
+    im2.putdata(encrypted_image_array)
+    #Save Image
+    name_array = unencrypted_image_file.split(".")
+    im2.save(".".join(name_array[:-1])+"_encrypted."+name_array[-1])
---
+++ Grant's Code/test_bench.py
@@ -0,0 +1,70 @@
+'''
+G. Meeser (grant@meeser.co.za)
+
+25/062020
+'''
+
+from PIL import Image
+from serial import Serial
+import functions
+
+chunk_size = 250
+
+def byte_xor(ba1, ba2):
+    # ba1 = ba1.to_bytes(1,byteorder='big')
+    # ba2 = ba2.to_bytes(1,byteorder='big')
+    return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)])
+
+def inputNumber(message):
+  while True:
+    try:
+       userInput = int(input(message))       
+    except ValueError:
+       print("Not an integer! Try again.")
+       continue
+    else:
+       return userInput 
+       break 
+
+#Get image and COM port data from user
+input_image_name = input("Path to image to use => ")
+com_port = input("Com port to use => ")
+#initialise com port
+try:
+    com = Serial(com_port,timeout=10,baudrate=115200)
+except:
+    print("Cannot open",com_port)
+    exit()
+
+#Golden Meause Key and Offset
+golden_key = (inputNumber("KEY (Integer) => ") % 256).to_bytes(1,'big')
+golden_offset = (inputNumber("Offset (Integer) => ") % 256).to_bytes(1,'big')
+
+#Load image and convert to flat byte array
+try:
+    input_image_array, tuple_count = functions.imageToArray(input_image_name)
+except:
+    print(input_image_name, "cannot be found or is an invalid image.")
+    exit()
+
+#Golden Meausre
+output_image_array_golden = []
+for i in range(0,len(input_image_array)):
+    offset = ((int.from_bytes(golden_offset,'big') + i) % 256).to_bytes(1,'big')
+    output_image_array_golden.append(byte_xor(input_image_array[i],byte_xor(golden_key,offset)))
+
+#Chunk and send data to FPGA
+output_image_array = []
+for i in range(0,len(input_image_array),chunk_size):
+    com.write(input_image_array[i:i+chunk_size]+b'111')
+    output_image_array.append(com.read(chunk_size))
+
+#Check received data against golden measure
+for i in range(0,len(input_image_array)):
+    if input_image_array[i] != output_image_array[i]:
+        raise Exception("Golden measure does not matched recieved data")
+
+print("Recieved data matches golden measure!")
+
+#Rebuild image from byte array
+functions.arrayToImage(output_image_array,input_image_name)
